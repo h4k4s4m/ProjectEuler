a6d8cadd73b38bdc0b92aa6886981e019d9e0a00
Sahm Samarghandi <sahm.samarghandi@gmail.com>
2024-09-17 19:10:09 -0700

    pushing optimized solution with scratchpad main.cpp

--- problem7.cpp
+++ problem7.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-int currentNumber = 2; // we increment this on the first loop so we start with 2, the first prime
-int primeCounter = 1;  // 2 is prime and we start at 3 so we count 1 to account for that
-bool isPrime = true;
 
 int main()
 {
+    int currentNumber = 1; // we increment this on the first loop so we start with 1, the first number we check is 3
+    int primeCounter = 1;  // 2 is prime and we start at 3 so we count 1 to account for that
 
     while (primeCounter != 10001)
     {
-        currentNumber++;
-        if (currentNumber % 2 == 0)
+        bool isPrime = true;
+        currentNumber += 2;
+
+        for (int i = 3; i * i <= currentNumber; i += 2)
         {
-            isPrime = false;
-        }
-        for (int i = 3; i*i <= currentNumber; i += 2)
-        {
-            if (isPrime == false)
-                break;
             if (currentNumber % i == 0)
             {
                 isPrime = false;
                 break;
             }
         }
+
         if (isPrime == true)
+        {
             primeCounter++;
-        isPrime = true;
+        }
     }
+
     std::cout << currentNumber;
 }
